2e76b3b9786c6e3d96d2d4541633e5b8e49dc6a3
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-08-27 07:17:11 +0100

    wip: define scope

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,40 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/*
+- https://www.erlang.org/doc/man/ets.html
+
+Table types:
+1. set - AVL
+2. ordered_set - CA Tree with write concurrency
+3. bag
+4. duplicate_bag
+*/
+
+// basic:
+// insertion
+// deletion
+// search
+
+// todo
+// :ets public api 
+// 
+
+// todo: internal node vs external node?
+struct WAVLNode<T: Ord> {
+    _value: T,
+    _height: usize,
+    _rank: usize,
+    _left: Option<Box<WAVLNode<T>>>,
+    _right: Option<Box<WAVLNode<T>>>,
 }
+
+pub struct WAVLTree<T: Ord> {
+    _root: Option<Box<WAVLNode<T>>>,
+    _length: usize,
+}
+
+/*
+impl WAVLNode {
+    fn rank_difference(&self, WAVLNode) -> usize {
+        // todo
+    }
+}
+*/
