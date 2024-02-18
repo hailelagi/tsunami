9b2868f7ad797c9f4d073a88ab1d3079a5dfd09c
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-02-18 22:52:09 +0100

    remove unused, add zerocopy helper
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,9 @@ const INITIAL_BLOCKS: usize = 10;
 // typical page size in bytes on linux x86-64
 const DEFAULT_BLOCK_SIZE: usize = 4096;
 
-pub mod tsunami;
-
 fn main() {
     let mem_limit = INITIAL_BLOCKS * DEFAULT_BLOCK_SIZE;
-    let memory = vec![0u8; mem_limit];
+    let mut memory = vec![0u8; mem_limit];
 
     tsunami::run(&mut memory)
 }
